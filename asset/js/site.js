1114ddc3b294a8ba9005dafb39f1ea83666e4929
Frank Nunez Rodriguez <franknunezrodriguez104@gmail.com>
2022-06-15 07:32:47 -0400

    Created tables

--- asset/js/site.js
+++ asset/js/site.js
@@ -1,7 +1,7 @@
 const uri = 'https://localhost:7015/api/Desempeno';
 let todos = [];
 
-function getItems() {
+function getConsultores() {
   fetch(uri)
     .then(response => response.json())
     .then(data => _displayItems(data))
@@ -13,7 +13,6 @@ function _displayItems(data) {
   select.innerHTML = '';
 
   //_displayCount(data.length);
-
   //const button = document.createElement('button');
 
   data.forEach(item => {
@@ -22,6 +21,65 @@ function _displayItems(data) {
     option.text = item.no_Usuario;
     select.appendChild(option);
   });
+}
+function getReceitas() {
 
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+    params ="datein = "+startDate;
+    params = "&dateend = "+endDate;
+    var select = document.getElementById('selected').options;
+    var txt = "";
+    var i;
+    for (i = 0; i < select.length; i++) {
+      
+        txt = txt + "&consultor = "+select[i].value;
+     
+    }
+    params= params+txt;
+    fetch(`${uri}/?${params}`)
+      .then(response => response.json())
+      .then(data => _displayItems(data))
+      .catch(error => console.error('Unable to get items.', error));
+}
+function _displayReceitas(data) {
+    const parent = document.getElementById('showResult');
+    parent.innerHTML = '';
+  
+    //_displayCount(data.length);
+    //const button = document.createElement('button');
+  
+    data.forEach(item => {
+      const option = document.createElement("option");
+      option.value = item.co_Usuario;
+      option.text = item.no_Usuario;
+      select.appendChild(option);
+    });
+
+    function tableCreate() {
+      var body = document.getElementsByTagName('body')[0];
+      var tbl = document.createElement('table');
+      
+      tbl.setAttribute('class', 'table caption-top');
+      data.f
+      var tbdy = document.createElement('tbody');
+      for (var i = 0; i < 3; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 2; j++) {
+          if (i == 2 && j == 1) {
+            break
+          } else {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode('\u0020'))
+            i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+            tr.appendChild(td)
+          }
+        }
+        tbdy.appendChild(tr);
+      }
+      tbl.appendChild(tbdy);
+      body.appendChild(tbl)
+    }
+    tableCreate();
   todos = data;
 }
